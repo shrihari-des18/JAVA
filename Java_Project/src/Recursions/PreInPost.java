dc9ff7aff8fdb502755c8315ca7b87289bb2c251
Shrihari Deshapande <shriharideshpande901@gmail.com>
2026-01-06 22:51:57 +0530

    Progress Updated

---
+++ Java_Project/src/Recursions/PreInPost.java
@@ -0,0 +1,16 @@
+package Recursions;
+
+public class PreInPost {
+    public static void pip(int n){
+        if(n == 0) return;
+        System.out.print(n);
+        pip(n-1);
+        System.out.print(n);
+        pip(n-1);
+        System.out.print(n);
+    }
+
+    public static void main(String[] args) {
+        pip(4);
+    }
+}
